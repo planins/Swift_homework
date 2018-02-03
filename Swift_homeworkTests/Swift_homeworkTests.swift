61d0b4a3facfce80cdfda14f07e21ec3b8690b52
Ivan Ganchev <planins@ivans-macbook-pro.local>
2018-02-03 23:24:02 +0200

    Initial Commit

---
+++ Swift_homeworkTests/Swift_homeworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_homeworkTests.swift
+//  Swift_homeworkTests
+//
+//  Created by Ivan Ganchev on 03/02/2018.
+//  Copyright © 2018 Ivan Ganchev. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_homework
+
+class Swift_homeworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
