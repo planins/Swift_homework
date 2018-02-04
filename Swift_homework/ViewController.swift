47c9461deaec85ecc39f6b4e8095bc7b90bfde1d
Ivan Ganchev <planins@ivans-macbook-pro.local>
2018-02-04 16:59:42 +0200

    Status bar set to light

--- Swift_homework/ViewController.swift
+++ Swift_homework/ViewController.swift
@@ -35,6 +35,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
 
